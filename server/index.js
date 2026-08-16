e73cabc1f5151121042469ef037d6ecf7acda76a
kajalfulatriya <Kajlfutariya@gmail.com>
2026-08-16 20:36:35 +0530

    url correction

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:3000",
-            "https://www.merndevelopmentkit.com"
+            "https://www.merndevelopmentkit.com",
         ],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
